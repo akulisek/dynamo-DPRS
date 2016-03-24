cebb88b413f65fc5b794f8f7d99667f0c40e298f
Rami Mtier <ramimtier@gmail.com>
2016-03-24 02:30:10 +0100

    Added aplication
    -gitignore do not have app bin, because when application need compiled
    java aplication
    -added swarm
    -added register

---
+++ DatabaseNode/application/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 0;
+		System.out.println("Hello world");
+		while (true)
+		{
+			i = i++;
+		}
+	}
+
+}

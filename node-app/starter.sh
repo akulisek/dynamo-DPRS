6c9feb8bf913243e049501f2390ca46be838e846
IceHax <iceforg68@gmail.com>
2016-03-28 18:48:30 +0200

    added dynamic rsyslog configuration on dynamo nodes

--- node-app/starter.sh
+++ node-app/starter.sh
@@ -1,1 +1,11 @@
-rsyslogd && catalina.sh run
+#get log server ip from consul
+IP=$(curl http://$CONSUL_IP:8500/v1/kv/logserver?raw)
+
+#set the ip in rsyslog config
+sed -i "s/172.17.0.2/$IP/g" /etc/rsyslog.d/test.conf
+
+#start rsyslog
+rsyslogd
+
+#start tomcat
+catalina.sh run

6262034ce409b567e885829e073f1ce74abcdf0f
IceHax <iceforg68@gmail.com>
2016-03-29 13:42:32 +0200

    logging-related updates
    changed ELK to newer version
    fixed logstash configuration, added elasticsearch output
    fixed rsyslog configuration
    fixed elk dockerfile, now using supervisor to run starter script

--- Logs/starter.sh
+++ Logs/starter.sh
@@ -1,5 +1,4 @@
+#!/usr/bin/env bash
+
 res=$(curl -X PUT -d $LOG_HOST_IP http://$CONSUL_IP:8500/v1/kv/logserver)
 echo $res
-
-#original entry
-/usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf

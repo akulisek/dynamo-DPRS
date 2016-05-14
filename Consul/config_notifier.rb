289c43f1e67b50d9180fd5741c811e0e2a95bdcb
IceHax <iceforg68@gmail.com>
2016-05-14 18:43:43 +0200

    Change watch scripts EOL to UNIX

--- Consul/config_notifier.rb
+++ Consul/config_notifier.rb
@@ -3,10 +3,15 @@
 
 require 'json'
 require 'net/http'
+<<<<<<< 7331329211d5677fc6d239c56cfe31bc198eb649
 require 'base64'
                                      
 encoded_conf = JSON.parse(STDIN.read)         
 dynamo_nodes = Base64.decode64(encoded_conf["Value"])
+=======
+
+dynamo_nodes = JSON.parse(STDIN.read)
+>>>>>>> Change watch scripts EOL to UNIX
 
 dynamo_nodes.each do |ip_port,data|
 	url = 'http://'+ip_port+'/node/update_configuration'

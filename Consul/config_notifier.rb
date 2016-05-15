3a50466ddfc32e6e2bf0dcbd0665f0107d7e6bbc
filip <filipjurcacko@gmail.com>
2016-05-16 01:36:18 +0200

    Config notifier now gets vms ip:port from healthcheck output XXX

--- Consul/config_notifier.rb
+++ Consul/config_notifier.rb
@@ -3,27 +3,33 @@
 
 require 'json'
 require 'net/http'
-require 'base64'
 
 IO.write('/tmp/watches.log', "config_notifier called\n", mode: 'a')
 
+#read stdin, unused
 input = STDIN.read
-IO.write('/tmp/watches.log', "input:"+input+"\n", mode: 'a') 
-if (!input) || (input.empty?) || input.length == 0 || input.start_with?('null')
-	#empty config, noone to notify	
-	exit 0
-end
-                                 
-encoded_conf = JSON.parse(input)
-IO.write('/tmp/watches.log', "encoded_conf:\n"+encoded_conf.to_json+"\n", mode: 'a')         
-dynamo_nodes = JSON.parse(Base64.decode64(encoded_conf["Value"]))
-IO.write('/tmp/watches.log', "decoded json:\n"+dynamo_nodes.to_json+"\n", mode: 'a') 
 
-dynamo_nodes.each do |ip_port,data|
-	url = 'http://'+ip_port+'/node/update_configuration'
-	uri = URI.parse(url)
-	http = Net::HTTP.new(uri.host, uri.port)
-	request = Net::HTTP::Get.new(uri.request_uri)
-	IO.write('/tmp/watches.log', "Get: "+url+"\n", mode: 'a')
-	response = http.request(request)
+#read passing dynamo node checks (alive nodes) to get vm ip:port XXX .. http://localhost:8500/v1/health/service/rails-image-3000?passing
+url = 'http://localhost:8500/v1/health/service/rails-image-3000?passing'
+uri = URI.parse(url)
+http = Net::HTTP.new(uri.host, uri.port)
+request = Net::HTTP::Get.new(uri.request_uri)
+response = http.request(request)
+IO.write('/tmp/watches.log', "Response: "+response.body+"\n", mode: 'a')
+
+checks = JSON.parse(response.body)
+checks.each do |check|
+#parse ip:port from check response - "Output":"HTTP GET http://192.168.99.102:19305: 200 OK 
+	output = check["Output"]
+	ip_port = output.split(' ')[2][0..-2]
+	if ip_port.start_with?('http')
+		#contact..
+		url = ip_port+'/node/update_configuration'
+		uri = URI.parse(url)
+		http = Net::HTTP.new(uri.host, uri.port)
+		request = Net::HTTP::Get.new(uri.request_uri)
+		IO.write('/tmp/watches.log', "Get: "+url+"\n", mode: 'a')
+		response = http.request(request)
+	end
 end
+
